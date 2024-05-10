3e0b8d72b4a367a79819f4c48c8f4e2e47189a42
Ramitphi <18ucs144@lnmiit.ac.in>
2024-05-11 04:42:29 +0530

    reminder frame

--- pages/api/frames/frames.ts
+++ pages/api/frames/frames.ts
@@ -1,5 +1,15 @@
+import { farcasterHubContext } from "frames.js/middleware";
 import { createFrames } from "frames.js/next/pages-router/server";
 
 export const frames = createFrames({
-  basePath: "/api/frames",
+  middleware: [
+    farcasterHubContext({
+      hubHttpUrl: "https://hubs.airstack.xyz",
+      hubRequestOptions: {
+        headers: {
+          "x-airstack-hubs": "1cd28db661ccd461cb3663ffb06821cf4",
+        },
+      },
+    }),
+  ],
 });
--- pages/api/frames/index.tsx
+++ pages/api/frames/index.tsx
@@ -1,22 +1,39 @@
 /* eslint-disable react/jsx-key */
 import { Button } from "frames.js/next/pages-router/server";
 import { frames } from "./frames";
+import Image from "next/image";
 
 const handleRequest = frames(async (ctx) => {
+  const convertToGoogleTimeFormat = (date: string) =>
+    new Date(date)
+      .toISOString()
+      .split("-")
+      .join("")
+      .split(":")
+      .join("")
+      .slice(0, 16)
+      .concat("Z");
+
+  const params = new URLSearchParams({
+    text: "Huddle01 Meeting",
+    dates: `${convertToGoogleTimeFormat(
+      "May 13, 2024, 6:30:00 PM"
+    )}/${convertToGoogleTimeFormat("May 13, 2024, 7:30:00 PM")}`,
+
+    sf: "true",
+    output: "xml",
+  }).toString();
+
   return {
-    image: (
-      <span>
-        Hello there: {ctx.pressedButton ? "✅" : "❌"}
-        {ctx.message?.inputText ? `, Typed: ${ctx.message?.inputText}` : ""}
-      </span>
-    ),
+    image: `${process.env.VERCEL_URL}/spaces.png`,
     buttons: [
-      <Button action="post">Click me</Button>,
-      <Button action="post" target="/next">
-        Next frame
+      <Button
+        action="link"
+        target={`https://calendar.google.com/calendar/u/0/r/eventedit?${params}`}
+      >
+        Add Reminder for Launch
       </Button>,
     ],
-    textInput: "Type something!",
   };
 });
 
